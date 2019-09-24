b0904ab43239626070389a8f78a84300b2e3fa68
tusharsuthar3 <55339478+tusharsuthar3@users.noreply.github.com>
2019-09-24 01:59:42 -0400

    intelliJ
    java code file

---
+++ Execute.java
@@ -0,0 +1,7 @@
+package com.code.practice;
+
+public class Execute {
+    public static void main(String[] args) {
+        System.out.println("Hi How Are you?");
+    }
+}
---
+++ MobilePhone.java
@@ -0,0 +1,23 @@
+package com.code.practice;
+
+public class MobilePhone {
+
+    //attributes
+    String company;
+    float wt;
+    float ht;
+
+
+    //Creating Variable
+
+    //behavior = method
+    public void call(){
+        //statement
+    }
+
+    public void sendMsg(){
+        //statement
+    }
+
+
+}
